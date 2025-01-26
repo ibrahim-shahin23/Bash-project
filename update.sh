7687ecfdb647ba13d2996e41df7a77aa016f2184
ibrahim-shahin23 <ibrahimsaaed38@gmail.com>
2025-01-27 00:02:44 +0200

    make a gui for update

--- update.sh
+++ update.sh
@@ -1,112 +1,100 @@
 #!/bin/bash
-if [[ ! "$(ls -A )" ]]; then
-	echo "There's no tables to update"
+
+if [[ ! "$(ls -A)" ]]; then
+    dialog --msgbox "There's no tables to update." 10 50
 else
     while true; do
-	    read -p "Please enter Table name to update : " TBname
-	    if [[ -z $TBname ]]; then
-		    echo "Invalid Table name."
-		else
-			break
-		fi	
-	done
-	    if [ -e "$TBname" ]; then
-        if [ -s "$TBname" ]; then
+        TBname=$(dialog --inputbox "Please enter the table name to update:" 10 50 3>&1 1>&2 2>&3)
+        if [[ $? -ne 0 ]]; then
+            dialog --msgbox "Operation canceled." 10 50
+            break
+        elif [[ -z $TBname ]]; then
+            dialog --msgbox "Invalid table name. Please try again." 10 50
+        else
+                if [[ -e "$TBname" ]]; then
+        if [[ -s "$TBname" ]]; then
             while true; do
-                echo "Update operation on table $TBname:"
-                echo "1. Update rows based on condition"
-                echo "2. cancel"
-                read -p "Choose an option (1 or 2): " option
-            
-            case $option in
-            1)
-                echo "Update operation on table $TBname:"
-                while true; do
-                echo "Available columns:"
-                columnSize=$(wc -l < .$TBname-metadata)
-                for ((i=1; i<=columnSize; i++))
-                    do
-                        colName=$(sed -n "$((i))p" .$TBname-metadata | cut -d: -f1)
-                        echo "$i) $colName"
-                    done	
-            
-                    read -p "Enter the column number to update: " updateCol
+                option=$(dialog --menu "Update operation on table $TBname:" 15 50 2 \
+                    1 "Update rows based on condition" \
+                    2 "Cancel" \
+                    3>&1 1>&2 2>&3)
 
-                    if [[ -z $updateCol || $updateCol -lt 1 || $updateCol -gt $columnSize ]]; then
-                    echo "Invalid column number. Please enter a valid number between 1 and $columnSize."
-                    else
-                    break
-                    fi
-                done
+                if [[ $? -ne 0 ]]; then
+                    dialog --msgbox "Operation canceled." 10 50
+                    exit
+                fi
 
-                columnMetadata=$(sed -n "${updateCol}p" .$TBname-metadata)
-                colName=$(echo $columnMetadata | cut -d: -f1)
-                colType=$(echo $columnMetadata | cut -d: -f2)
-                colPkCheck=$(echo $columnMetadata | cut -d: -f3)
+                case $option in
+                1)
+                    columnSize=$(wc -l < ".$TBname-metadata")
+                    colMenu=""
+                    for ((i = 1; i <= columnSize; i++)); do
+                        colName=$(sed -n "$i p" ".$TBname-metadata" | cut -d: -f1)
+                        colMenu+="$i $colName "
+                    done
 
-                while true; do
-                    read -p "Enter the new value to update: " val
-                    if [[ $colPkCheck == "pk" ]]; then
-
-                    if [[ $(grep -c "$val:" "$TBname") -ne 0 ]]; then
-                        echo "Primary key value '$val' already exists. Please enter a unique value."
+                    updateCol=$(dialog --menu "Select the column to update:" 15 50 $columnSize $colMenu 3>&1 1>&2 2>&3)
+                    if [[ $? -ne 0 ]]; then
                         continue
                     fi
-                    fi
-                    if [[ $colType == "int" && ! $val =~ ^[0-9]+$ ]]; then
-                    echo "Invalid value. Please enter a valid integer."
-                    continue
-                    elif [[ $colType == "string" && ! $val =~ ^[a-zA-Z0-9_]+$ ]]; then
-                    echo "Invalid value. Please enter a valid string (alphanumeric and underscores only)."
-                    continue
-                    else
-                    break
-                    fi
-                done
 
-                while true; do
-                    read -p "Enter the column number to filter for update: " filterCol
+                    columnMetadata=$(sed -n "${updateCol}p" ".$TBname-metadata")
+                    colName=$(echo $columnMetadata | cut -d: -f1)
+                    colType=$(echo $columnMetadata | cut -d: -f2)
+                    colPkCheck=$(echo $columnMetadata | cut -d: -f3)
 
-                    if [[ -z $filterCol || $filterCol -lt 1 || $filterCol -gt $columnSize ]]; then
-                    echo "Invalid column number for filtering. Please enter a valid number between 1 and $columnSize."
-                    else
-                    break
-                    fi
-                done
-
-                while true; do
-                    read -p "Enter the value to filter by: " filterVal
-
-                    if [[ -z $filterVal ]]; then
-                    echo "Invalid value. Please enter a valid value to filter by."
-                    else
-                        if [[ `cut -d: -f$filterCol $TBname | grep -c ^$filterVal$` -eq 0 ]]
-                        then
-                            echo "value doesn't exist"
+                    while true; do
+                        val=$(dialog --inputbox "Enter the new value for $colName:" 10 50 3>&1 1>&2 2>&3)
+                        if [[ $? -ne 0 ]]; then
+                            break
+                        elif [[ $colPkCheck == "pk" && $(grep -c "^$val:" "$TBname") -ne 0 ]]; then
+                            dialog --msgbox "Primary key value '$val' already exists. Please enter a unique value." 10 50
+                            continue
+                        elif [[ $colType == "int" && ! $val =~ ^[0-9]+$ ]]; then
+                            dialog --msgbox "Invalid value. Please enter a valid integer." 10 50
+                            continue
+                        elif [[ $colType == "string" && ! $val =~ ^[a-zA-Z0-9_]+$ ]]; then
+                            dialog --msgbox "Invalid value. Please enter a valid string (alphanumeric and underscores only)." 10 50
+                            continue
                         else
-                        break
+                            break
                         fi
+                    done
+
+                    filterCol=$(dialog --menu "Select the column to filter by:" 15 50 $columnSize $colMenu 3>&1 1>&2 2>&3)
+                    if [[ $? -ne 0 ]]; then
+                        continue
                     fi
-                done
 
-                awk -F: -v filterCol="$filterCol" -v filterVal="$filterVal" -v updateCol="$updateCol" -v newValue="$val" \
-                    'BEGIN {OFS=":"} $filterCol == filterVal { $updateCol = newValue } { print $0 }' "$TBname" > tempFile && mv tempFile "$TBname"
+                    filterVal=$(dialog --inputbox "Enter the value to filter by:" 10 50 3>&1 1>&2 2>&3)
+                    if [[ $? -ne 0 ]]; then
+                        continue
+                    elif [[ $(cut -d: -f"$filterCol" "$TBname" | grep -c "^$filterVal$") -eq 0 ]]; then
+                        dialog --msgbox "No rows found with the specified filter value." 10 50
+                        continue
+                    fi
 
-                echo "Table updated successfully."
-                ;;
-            2)
-                break
-                ;;
-            *)
-                echo "Invalid option"
-                ;;
-            esac
+                    awk -F: -v filterCol="$filterCol" -v filterVal="$filterVal" -v updateCol="$updateCol" -v newValue="$val" \
+                        'BEGIN {OFS=":"} $filterCol == filterVal { $updateCol = newValue } { print $0 }' "$TBname" > tempFile && mv tempFile "$TBname"
+
+                    dialog --msgbox "Table updated successfully." 10 50
+                    ;;
+                2)
+                    break
+                    ;;
+                *)
+                    dialog --msgbox "Invalid option." 10 50
+                    ;;
+                esac
             done
-            else
-            echo "$TBname table is empty"
+        else
+            dialog --msgbox "The table $TBname is empty." 10 50
         fi
-	else
-		echo "$TBname Table doesn't exist."
-	fi
+    else
+        dialog --msgbox "Table $TBname does not exist." 10 50
+    fi
+
+        fi
+    done
 
 fi
